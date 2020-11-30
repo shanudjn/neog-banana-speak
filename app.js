ebbe2aba581e572a38ff6ab7af05b67790d10bb2
Md Shahazad Uddin <shanudjn@gmail.com>
2020-11-30 12:54:56 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var textInput = document.querySelector('#text-input');
 var btnTranslate = document.querySelector('#btn-translate');
+var textOutput = document.querySelector('#text-output');
 
 btnTranslate.addEventListener('click', function handleClick() {
     console.log('Clicked!!!');
-    console.log(textInput.value)
-
+    console.log(textInput.value);
+    let inputTextValue = textInput.value;
+    textOutput.innerText = inputTextValue;
 });
-console.log(textInput);
+
 
 //console.log(textInput.value)
