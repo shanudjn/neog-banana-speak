be381a492a68ee258ed1037703c50d32dc62274b
Md Shahazad Uddin <shanudjn@gmail.com>
2020-11-30 11:50:33 +0530

    understanding queryselector

---
+++ app.js
@@ -0,0 +1,11 @@
+var textInput = document.querySelector('#text-input');
+var btnTranslate = document.querySelector('#btn-translate');
+
+btnTranslate.addEventListener('click', function handleClick() {
+    console.log('Clicked!!!');
+    console.log(textInput.value)
+
+});
+console.log(textInput);
+
+//console.log(textInput.value)
